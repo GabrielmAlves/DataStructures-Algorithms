c2f24b13a0c6729a317e98b21598090b08ea4777
GabrielmAlves <gabrielmalves10@gmail.com>
2025-02-25 06:10:51 -0300

    add variables and functions to header file

--- AlgoritmosApp/Algoritmos/QuickSort.h
+++ AlgoritmosApp/Algoritmos/QuickSort.h
@@ -1,5 +1,15 @@
 #pragma once
+#include <vector>
+
 class QuickSort
 {
+public:
+	int pivot;
+	vector<int> originalVector;
+	vector<int> smallerVector;
+	vector<int> biggerVector;
+
+	int partition(vector<int>& vec, int low, int high);
+	void quickSort(vector<int> &vec);
 };
 
